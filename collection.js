962b7b26d10b6860c586c3c8d8f92b6c28d36c4c
victor <ukyz@icloud.com>
2019-02-11 20:31:19 +0100

    Create lib and clean some

--- collection.js
+++ collection.js
@@ -1,27 +1,27 @@
-const {Database, aql} = require('arangojs');
+const {Database} = require('arangojs');
 
 const db = new Database();
 
 const main = async () => {
-  /*const collection = db.collection('lemmeRelations');
-  await collection.drop();*/
-  /*await graph.create({
+	/* Const collection = db.collection('lemmeRelations');
+  await collection.drop(); */
+	/* await graph.create({
     edgeDefinitions: [{
       collection: 'synRelations',
       from: ['words'],
       to: ['words']
     }]
-  });*/
-  const graph = db.graph('jeuDeMot');
-  //const collection = db.collection("lemmeRelations");
-  //console.log(collection);
-  await graph.addEdgeDefinition({
-    collection: 'lemmeRelations',
-    from: ['words'],
-    to: ['words']
-  });
-  console.log(await db.listDatabases());
-  console.log(await db.listCollections());
+  }); */
+	const graph = db.graph('jeuDeMot');
+	// Const collection = db.collection("lemmeRelations");
+	// console.log(collection);
+	await graph.addEdgeDefinition({
+		collection: 'lemmeRelations',
+		from: ['words'],
+		to: ['words']
+	});
+	console.log(await db.listDatabases());
+	console.log(await db.listCollections());
 };
 
 main();
