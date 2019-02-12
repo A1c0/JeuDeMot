9bdfd89b0960e951c6b0271d31d5708d68e9fc6a
victor <ukyz@icloud.com>
2019-02-12 12:41:25 +0100

    Add results out functions

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
 const {Database} = require('arangojs');
 const {
 	wordMatch,
-	lemmeRelationForWord,
-	wordMatchId
+	wordMatchId,
+	synRelationForWord,
+	familyRelationForWord
 } = require('./lib/queries.js');
 
 const db = new Database();
 
+const tabSentence = [
+	'faire randonnée',
+	'voyager lille'
+];
+
 const tabWord = [
-	'fait'
+	'faire',
+	'randonnée',
+	'voyager',
+	'lille'
 ];
 
 const computeRelScore = async tabWord =>
@@ -20,7 +29,10 @@ const computeRelScore = async tabWord =>
 				res.score = ((res.ww * wwMax) * 0.5) + ((res.wRel * wRelMax) * 0.5);
 			});
 		});
-		resolve(tabWord.map(x => x.res.sort((a, b) => b.score - a.score)));
+		tabWord.forEach(word => {
+			word.res.sort((a, b) => b.score - a.score);
+		});
+		resolve(tabWord);
 	});
 
 const getMaxScore = async tabWord =>
@@ -31,6 +43,7 @@ const getMaxScore = async tabWord =>
 				if (res.ww > max.ww) {
 					max.ww = res.ww;
 				}
+
 				if (res.wRel > max.wRel) {
 					max.wRel = res.wRel;
 				}
@@ -59,21 +72,21 @@ const main = async tabWord => {
 			});
 		}
 	}, Promise.resolve());
-	getMaxScore(tabRes).then(resWithMax => {
-		computeRelScore(resWithMax).then(resWithScore => {
-			resWithScore.forEach(word => {
-				console.log(word);
+	return new Promise(resolve => {
+		getMaxScore(tabRes).then(resWithMax => {
+			computeRelScore(resWithMax).then(resWithScore => {
+				resolve(resWithScore);
 			});
 		});
 	});
 };
 
 const getClosestWord = async wordID => {
-	const resRelSyn = await lemmeRelationForWord(wordID, db);
-	/* Const resRelFamily = await familyRelationForWord(wordID);
-	const rels = resRelSyn._result.concat(resRelFamily._result); */
+	const resRelSyn = await synRelationForWord(wordID, db);
+	const resRelFamily = await familyRelationForWord(wordID, db);
+	const rels = resRelSyn._result.concat(resRelFamily._result);
 	const tabRel = [];
-	await resRelSyn.reduce(async (promise, res) => {
+	await rels.reduce(async (promise, res) => {
 		await promise;
 		const wRef1 = await wordMatchId(res._from, db);
 		let wRef2 = await wordMatchId(res._to, db);
@@ -87,8 +100,93 @@ const getClosestWord = async wordID => {
 		}
 	}, Promise.resolve());
 	return tabRel.sort((a, b) => b.wRel - a.wRel)
-		.splice(0, 15);
+		.splice(0, 10);
 };
 
-main(tabWord);
+const getTagsBySentence = async (tabSentence, tabRelWord) => {
+	return new Promise(resolve => {
+		const tabRes = [];
+		tabSentence.forEach(sentence => {
+			const tabResToPush = {sentence, tags: []};
+			sentence.toString().split(' ')
+				.forEach(wordFromSentence => {
+					getIndexWordInTabRel(wordFromSentence, tabRelWord)
+						.then(index => {
+							if (index >= 0) {
+								tabResToPush.tags.push(wordFromSentence);
+								tabRelWord[index].res
+									.forEach(a => tabResToPush.tags.push(a.word));
+							}
+						});
+				});
+			tabRes.push(tabResToPush);
+		});
+		resolve(tabRes);
+	});
+};
+
+const getIndexWordInTabRel = async (word, tabRelWord) => {
+	return new Promise(resolve => {
+		for (let i = 0; i < tabRelWord.length; i++) {
+			if (tabRelWord[i].word === word) {
+				resolve(i);
+			}
+		}
+
+		resolve(-1);
+	});
+};
+
+const listSentenceByTag = async tabSentence => {
+	const tabRes = [];
+	await tabSentence.reduce(async (promise, sentenceItem) => {
+		await promise;
+		await sentenceItem.tags.forEach(tagFromSentence => {
+			getIndexTag(tagFromSentence, tabRes).then(index => {
+				if (index >= 0) {
+					tabRes[index].sentences.push(sentenceItem.sentence);
+				} else {
+					tabRes.push(
+						{tag: tagFromSentence, sentences: [sentenceItem.sentence]});
+				}
+			});
+		});
+	}, Promise.resolve());
+	return tabRes;
+};
+
+const getIndexTag = async (tag, tabTag) => {
+	return new Promise(resolve => {
+		for (let i = 0; i < tabTag.length; i++) {
+			if (tag === tabTag[i].tag) {
+				resolve(i);
+			}
+		}
+
+		resolve(-1);
+	});
+};
+
+const sortAndFilter = async tabWord => {
+	return new Promise(resolve => {
+		tabWord.forEach(word => {
+			word.res = word.res.splice(0, 3)
+				.filter(res => res.ww > word.ww);
+		});
+		resolve(tabWord);
+	});
+};
+
+const test = async tabWord => {
+	await getTagsBySentence(tabSentence,
+		await sortAndFilter(await main(tabWord)))
+		.then(async resBySentence => {
+			console.log(JSON.stringify(resBySentence, null, 1));
+			await listSentenceByTag(resBySentence).then(async resByTag => {
+				console.log(JSON.stringify(resByTag, null, 1));
+			});
+		});
+};
+
+test(tabWord);
 
